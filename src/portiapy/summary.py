927b232cbb0d959725cd079101fb24cbab3ef237
Niccolas Costa <niccolas.costa@agriness.com>
2019-05-23 08:29:38 -0300

    Implementando os convertes para os accepts atuais e atualizando os request do summary para utilizar os converter

--- src/portiapy/summary.py
+++ src/portiapy/summary.py
@@ -3,12 +3,13 @@
 #####################################
 
 # Libraries
-from enum import Enum             # Support for enumerations
-from io import StringIO           # Read and write strings as files
-import pandas                     # Data analysis tools for Python
-import portiapy.utils as utils
+from enum import Enum  # Support for enumerations
 
+import portiapy.utils as utils
 # Enums
+from portiapy.utils import response_convert
+
+
 class SummaryStrategies(Enum):
     PER_MINUTE = 1
     PER_HOUR   = 2
@@ -39,24 +40,12 @@ def resolveStrategy(strategy):
 ########################################
 def queryByPortSensor(portiaConfig, edgeId, port, sensor, strategy=SummaryStrategies.PER_HOUR, interval=1, params={ 'from': None, 'to': None, 'order': None, 'precision': 'ms', 'offset': 0, 'min': True, 'max': True, 'sum': True, 'avg': True, 'median': True, 'mode': False, 'stddev': False, 'spread': False }):
     """Returns a pandas data frame with the portia select resultset"""
-
-    header = {'Accept': 'text/csv'}
+    type =  portiaConfig['Accept']
+    header = {'Accept': type}
     endpoint = '/summary/device/{0}/port/{1}/sensor/{2}/{3}/{4}{5}'.format( edgeId, port, sensor, resolveStrategy(strategy), interval, utils.buildGetParams(params) )
     response = utils.httpGetRequest(portiaConfig, endpoint, header)
 
-    if response.status_code == 200:
-        try:
-
-            dimensionSeries = pandas.read_csv( StringIO(response.text), sep=';' )
-            if portiaConfig['debug']:
-                print( '[portia-debug]: {0} rows'.format( len(dimensionSeries.index) ) )
-
-            return dimensionSeries
-
-        except Exception as err:
-            raise Exception( 'couldn\'t create pandas data frame: {}'.format(err) )
-    else:
-        raise Exception('couldn\'t retrieve data')
+    return response_convert[type](response, portiaConfig)
 
 ########################################
 # devicePortSensorDimensions
@@ -66,20 +55,9 @@ def queryByPortSensor(portiaConfig, edgeId, port, sensor, strategy=SummaryStrate
 def queryByPortSensorDimension(portiaConfig, edgeId, port, sensor, dimensionCode, strategy=SummaryStrategies.PER_HOUR, interval=1, params={ 'from': None, 'to': None, 'order': None, 'precision': 'ms', 'offset': 0, 'min': True, 'max': True, 'sum': True, 'avg': True, 'median': True, 'mode': False, 'stddev': False, 'spread': False }):
     """Returns a pandas data frame with the portia select resultset"""
 
-    header = {'Accept': 'text/csv'}
+    type = portiaConfig['Accept']
+    header = {'Accept': type}
     endpoint = '/summary/device/{0}/port/{1}/sensor/{2}/dimension/{3}/{4}/{5}{6}'.format( edgeId, port, sensor, dimensionCode, resolveStrategy(strategy), interval, utils.buildGetParams(params) )
     response = utils.httpGetRequest(portiaConfig, endpoint, header)
 
-    if response.status_code == 200:
-        try:
-
-            dimensionSeries = pandas.read_csv( StringIO(response.text), sep=';' )
-            if portiaConfig['debug']:
-                print( '[portia-debug]: {0} rows'.format( len(dimensionSeries.index) ) )
-
-            return dimensionSeries
-
-        except Exception as err:
-            raise Exception( 'couldn\'t create pandas data frame: {}'.format(err) )
-    else:
-        raise Exception('couldn\'t retrieve data')
+    return response_convert[type](response, portiaConfig)
